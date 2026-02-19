a2d3d902f204c5257fd0565c653e9b9980dbaf8d
mugunthanarjunan-glitch <mugunthanarjunan@gmail.com>
2026-02-19 15:48:09 +0530

    MongoDB connection done on server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-
+const mongoose = require("mongoose")
 
 const app = express()
 
@@ -8,7 +9,9 @@ app.use(cors())
 app.use(express.json())
 
 
-
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {console.log("MongoDB is connected")})
+    .catch(err => console.log("error on connect"))
 
 const PORT=7000
 
