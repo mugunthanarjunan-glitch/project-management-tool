6e0252171d8505875dc2d46c592adfc6eaa9039b
mugunthanarjunan-glitch <mugunthanarjunan@gmail.com>
2026-02-19 15:27:34 +0530

    port created for backend

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,17 @@
 const express = require("express")
 const cors = require("cors")
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const PORT=7000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port 7000 http://localhost:${PORT}/`)
+})
